cb53a315f594b991b1514a57239f3dd252c0b1f1
hyungrae0907 <hyungrae0907@gmail.com>
2025-09-28 15:07:04 +0900

    add: ex01

---
+++ khr/ex01.py
@@ -0,0 +1,9 @@
+var1 = 1
+var2 = "2"
+var3 = [1,2,"3"]
+var4 = {"key" : 1}
+
+print(var1)
+print(var2)
+print(var3)
+print(var4.get("key"))
--- practice.py
+++ practice.py
@@ -6,7 +6,7 @@ from operator import itemgetter
 import os
 
 load_dotenv()
-os.environ["LANGCHAIN_TRACING_V2"] = 'true'
+os.environ["LANGCHAIN_TRACING_V2"] = 'false'
 os.environ["LANGCHAIN_ENDPOINT"] = os.getenv("LANGCHAIN_ENDPOINT")
 os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT")
 os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
