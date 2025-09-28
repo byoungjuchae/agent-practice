3dcbfc01432eaba3587a1aa746e5c671eb79885f
hyerim <hyerimiya1216@gmail.com>
2025-09-28 15:14:27 +0900

    first commit : ex01

---
+++ hyerim/ex01.py
@@ -0,0 +1,10 @@
+list = [one, two, three]
+for index in list :
+  print(index)
+
+number = 0
+while number < 10:
+  number = number + 1
+  print(f"지금 숫자는{number}")
+  if number == 10:
+    print("숫자 끝")
